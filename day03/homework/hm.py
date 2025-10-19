f62d5a7ea98994c5648f0d0f684c76999e1e9218
vanojinashvili <vanojinashvili1@gmail.com>
2025-10-19 14:38:41 +0400

    “commit”

---
+++ day03/homework/hm.py
@@ -0,0 +1,21 @@
+number = 50
+number = 75
+number = 100
+print(number)
+
+# print:hello:
+# input(enter your name'
+# print "hello"
+#print "hi'
+#20$30
+#1age = error
+
+number1 = 3
+age = 15
+print(3 * 15)
+
+print(10 / 2)
+print(5 * 10)
+print(25//8)
+print(5 + 10)
+print(8 - 3)
---
+++ day05/homework/hw.py
@@ -0,0 +1,25 @@
+number1 = int(input("random number 1 - 100:"))
+number2 = int(input("enter your age here:"))
+
+print(number1 + number2)
+
+
+
+number3 = input("random number 1 - 100:")
+number4 = input("enter your age here:")
+
+print(number3 + number4)
+
+user_name = input("enter your name here:")
+user_surname = input("your surname here:")
+user_age = int(input("enter your age here:"))
+
+print("hello",user_name ,user_surname,"you are",user_age,"years old")
+
+number5 = int(input("შეიყვანე პირველი რიცხვი: "))
+number6 = int(input("შეიყვანე მეორე რიცხვი: "))
+
+print("ნაშთიანად გაყოფის შედეგია:", number5 / number6)
+
+programing_academy = input("რომელი პროგრამირების აკადემია მიგაჩნია საუკეთესოდ?")
+print(programing_academy)
